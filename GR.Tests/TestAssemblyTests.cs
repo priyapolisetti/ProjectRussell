6cd90bc260d31ed1fbbfae5e66590b7b135c86b0
Priyanka Polisetti <priyapolisetty189@gmail.com>
2020-06-18 14:10:54 -0400

    Added fixes and test cases

---
+++ GR.Tests/TestAssemblyTests.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GR.Tests
+{
+    public class TestAssemblyTests
+    {
+        private readonly Program _app;
+
+        public TestAssemblyTests()
+        {
+            _app = new Program
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 0, Quality = 0},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 50},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETP concert",
+                        SellIn = 5,
+                        Quality = 50
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a D498FJ9FJ2N concert",
+                        SellIn = 10,
+                        Quality = 30
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a FH38F39DJ39 concert",
+                        SellIn = 5,
+                        Quality = 33
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a I293JD92J44 concert",
+                        SellIn = 6,
+                        Quality = 27
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a O2848394820 concert",
+                        SellIn = 1,
+                        Quality = 13
+                    },
+                    new Item
+                    {
+                        Name = "Backstage passes to a DEEEADMEEET concert",
+                        SellIn = 0,
+                        Quality = 25
+                    },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6},
+
+                     new Item {Name = "Conjured Mana Cake 2", SellIn = -1, Quality = 3},
+
+                      new Item {Name = "Conjured Mana Cake 3", SellIn = 0, Quality = 6}
+                }
+            };
+
+            _app.UpdateInventory();
+        }
+
+        [Fact]
+        public void DexterityVest_Quality_ShouldBeZero()
+        {
+            Assert.Equal(0, _app.Items.First(x => x.Name == "+5 Dexterity Vest").Quality);
+        }
+
+        [Fact]
+        public void Backstage_Quality_ShouldBeZero()
+        {
+            Assert.Equal(0, _app.Items.First(x => x.Name == "Backstage passes to a DEEEADMEEET concert").Quality);
+        }
+
+        [Fact]
+        public void Backstage_Quality_ShouldIncreaseByThree()
+        {
+            Assert.Equal(16, _app.Items.First(x => x.Name == "Backstage passes to a O2848394820 concert").Quality);
+        }
+
+        [Fact]
+        public void Backstage_Quality_ShouldIncreaseByTwo()
+        {
+            Assert.Equal(29, _app.Items.First(x => x.Name == "Backstage passes to a I293JD92J44 concert").Quality);
+        }
+
+        
+        [Fact]
+        public void Backstage_Quality_ShouldIncreaseByThreeForFiveDays()
+        {
+            Assert.Equal(36, _app.Items.First(x => x.Name == "Backstage passes to a FH38F39DJ39 concert").Quality);
+        }
+
+        
+        [Fact]
+        public void Backstage_Quality_ShouldIncreaseByTwoForTenDays()
+        {
+            Assert.Equal(32, _app.Items.First(x => x.Name == "Backstage passes to a D498FJ9FJ2N concert").Quality);
+        }
+
+        
+        [Fact]
+        public void Backstage_Quality_ShouldIncreaseByOne()
+        {
+            Assert.Equal(21, _app.Items.First(x => x.Name == "Backstage passes to a TAFKAL80ETC concert").Quality);
+        }
+
+        [Fact]
+        public void Conjured_Quality_ShouldDecreaseByTwo()
+        {
+            Assert.Equal(4, _app.Items.First(x => x.Name == "Conjured Mana Cake").Quality);
+        }
+
+        [Fact]
+        public void Conjured_Quality_ShouldDecreaseByFour()
+        {
+            Assert.Equal(0, _app.Items.First(x => x.Name == "Conjured Mana Cake 2").Quality);
+        }
+
+        [Fact]
+        public void Conjured_Quality_ShouldDecreaseByFourForZeroDays()
+        {
+            Assert.Equal(2, _app.Items.First(x => x.Name == "Conjured Mana Cake 3").Quality);
+        }
+
+        [Fact]
+        public void AgedBrie_Quality_ShouldBeFifty()
+        {
+            Assert.Equal(50, _app.Items.First(x => x.Name == "Aged Brie").Quality);
+        }
+
+        [Fact]
+        public void Backstage_Quality_ShouldBeFifty()
+        {
+            Assert.Equal(50, _app.Items.First(x => x.Name == "Backstage passes to a TAFKAL80ETP concert").Quality);
+        }
+    }
+}
---
+++ GR/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace GR
+{
+    public class Program
+    {
+        public IList<Item> Items;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+
+            var app = new Program()
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
+            };
+
+            app.UpdateInventory();
+
+            var filename = $"inventory_{DateTime.Now:yyyyddMM-HHmmss}.txt";
+            var inventoryOutput = JsonConvert.SerializeObject(app.Items, Formatting.Indented);
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename), inventoryOutput);
+
+            Console.ReadKey();
+        }
+
+        public void UpdateInventory()
+        {
+            Console.WriteLine("Updating inventory");
+            foreach (var item in Items)
+            {
+                Console.WriteLine(" - Item: {0}", item.Name);
+                if (item.Name != "Aged Brie" && !item.Name.Contains("Backstage passes"))
+                {
+                    if (item.Quality > 0 )
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            if (item.Name.Contains("Conjured Mana Cake"))
+                            {
+                                item.Quality = Math.Max(0, item.Quality - 2);
+                            }
+                            else
+                            {
+                                item.Quality = item.Quality - 1;
+                            }
+                        }
+                    }
+                }
+                else // if Aged brie or backstage
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+
+                        if (item.Name.Contains("Backstage passes"))
+                        {
+                            if (item.SellIn < 11)
+                            {
+                                item.Quality = Math.Min(50, item.Quality + 1);
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                item.Quality = Math.Min(50, item.Quality + 1);
+                            }
+                        }
+                    }
+                }
+
+                // sellIn decreasing
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+
+                if (item.SellIn >= 0)
+                    continue;
+
+                // sellin < 0
+                if (item.Name != "Aged Brie")
+                {
+                    if (item.Name.Contains("Backstage passes"))
+                    {
+                        if (item.Quality <= 0)
+                            continue;
+                        item.Quality = 0;
+                    }
+                    else
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            if (item.Name.Contains("Conjured Mana Cake"))
+                            {
+                                item.Quality = Math.Max(0, item.Quality - 2);
+                            }
+                            else
+                            {
+                                item.Quality = Math.Max(0,item.Quality - 1);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+            Console.WriteLine("Inventory update complete");
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
